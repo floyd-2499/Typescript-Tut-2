6b9d8d5099b7a699090f7f63233f3400a60bac43
Floyd Manuel <floydmanuel011@gmail.com>
2021-10-04 22:12:05 +0530

    about union

--- Vid-9/app.js
+++ Vid-9/app.js
@@ -6,6 +6,7 @@ var random = ["abc", 123, false];
 // what if you have many
 // so 
 var random2 = ["abc", 123, false];
+// **********************************************************
 // Tuple - vid 10
 // Tuple means - you specify all 
 var role = ['admin', 'manager', 2, 5];
@@ -16,3 +17,41 @@ role[1] = "hello"; // it can pass
 var role2 = ['admin', 'manager', 2, 5];
 role2.push(true);
 console.log(role2);
+// **********************************************************
+// Enum - vid-11
+// enum - gives number to every string/value 
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 0] = "admin";
+    Role[Role["manager"] = 1] = "manager";
+    Role[Role["read_only_user"] = 2] = "read_only_user";
+})(Role || (Role = {}));
+// this is used cz when you jus type Role for further reference it automatically pops up - Role.
+console.log(Role);
+console.log(Role.admin, "admin");
+console.log(Role.manager, "manager");
+// *********************************************************
+// ANY type in TypeScript
+// any means any kind of data type can come...
+var data = 10;
+data = "hello";
+// you can also define any as array....   but in js has more chances of getting error
+var data2 = [20, "yo yo"];
+// ******************************************************************
+// union Type in Typescript
+// union type means assigning two or more types
+var data10 = 10; //example
+function combine(a, b) {
+    // return a+b  this shows error so try below
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else {
+        return a.toString() + b.toString();
+    }
+}
+console.log(combine(10, 20));
+console.log(combine("Floy", "Manuel"));
+// what is alternate???
+// use any???
+//  better use union  than any cz some benefits you won't receive in any.
--- Vid-9/app.ts
+++ Vid-9/app.ts
@@ -9,7 +9,7 @@ let random:[string, number, boolean] = ["abc", 123, false];
 // so 
 let random2:any[] = ["abc", 123, false];
 
-
+// **********************************************************
 
 // Tuple - vid 10
 // Tuple means - you specify all 
@@ -22,4 +22,52 @@ role[1] = "hello"; // it can pass
 let role2:[string, string, number, number, boolean?] = ['admin', 'manager', 2 , 5];
 role2.push(true);
 console.log(role2)
+
+// **********************************************************
+
+// Enum - vid-11
+// enum - gives number to every string/value 
+
+enum Role{
+    admin, manager, read_only_user
+}
+
+// this is used cz when you jus type Role for further reference it automatically pops up - Role.
+console.log(Role)
+console.log(Role.admin, "admin")
+console.log(Role.manager, "manager")
+
+// *********************************************************
+
+// ANY type in TypeScript
+// any means any kind of data type can come...
+let data:any = 10;
+data = "hello";
+// you can also define any as array....   but in js has more chances of getting error
+let data2:any[] = [20, "yo yo"];
+
+// ******************************************************************
+
+// union Type in Typescript
+// union type means assigning two or more types
+let data10 : number | string = 10; //example
+
+function combine(a:number | string, b:number |string){
+    // return a+b  this shows error so try below
+
+    if(typeof a === 'number' && typeof b === 'number'){
+        return a+b;
+    }else {
+        return a.toString() + b.toString();
+    }
+}
+console.log(combine(10,20))
+console.log(combine("Floy","Manuel"))
+
+// what is alternate???
+
+// use any???
+
+//  better use union  than any cz some benefits you won't receive in any.
+
 

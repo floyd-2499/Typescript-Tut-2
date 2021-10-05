f171d92f292dad45e180d4e85f8e665eff0ae464
Floyd Manuel <floydmanuel011@gmail.com>
2021-10-05 23:35:23 +0530

    compile multiple files- vid 20

--- Vid-20/work.js
+++ Vid-20/work.js
@@ -1,1 +1,6 @@
+"use strict";
 // Compile and Use Multiple Files 
+console.log("hello");
+// How you compile both Work.ts and dummy.ts ??? 
+// 1. try individual //but it will be lengthy so try next
+// 2.   @terminal.... tsc -init
--- Vid-20/work.ts
+++ Vid-20/work.ts
@@ -1,1 +1,7 @@
 // Compile and Use Multiple Files 
+console.log("hello");
+
+// How you compile both Work.ts and dummy.ts ??? 
+// 1. try individual //but it will be lengthy so try next
+// 2.   @terminal.... tsc -init
+ 

33ba969d4f400df4416a25ade37b9b76ed648313
Floyd Manuel <floydmanuel011@gmail.com>
2021-10-05 17:12:06 +0530

    Function Tye and Void Function

--- Vid-9/app.js
+++ Vid-9/app.js
@@ -4,22 +4,27 @@ var arr = ["php", "js", "ts", "node"]; //string array - :string[]
 // let randomNum = ["abc", 123, false] //all mix so
 var random = ["abc", 123, false];
 // what if you have many
-// so 
+// so
 var random2 = ["abc", 123, false];
 // **********************************************************
 // Tuple - vid 10
-// Tuple means - you specify all 
-var role = ['admin', 'manager', 2, 5];
+// Tuple means - you specify all
+var role = ["admin", "manager", 2, 5];
 // but if you wanna edit it
 // role[0] = 123; //error cz only string  can pass at position[0]
 role[1] = "hello"; // it can pass
 // to push
-var role2 = ['admin', 'manager', 2, 5];
+var role2 = [
+    "admin",
+    "manager",
+    2,
+    5,
+];
 role2.push(true);
 // console.log(role2)
 // **********************************************************
 // Enum - vid-11
-// enum - gives number to every string/value 
+// enum - gives number to every string/value
 var Role;
 (function (Role) {
     Role[Role["admin"] = 0] = "admin";
@@ -43,7 +48,7 @@ var data2 = [20, "yo yo"];
 var data10 = 10; //example
 function combine(a, b) {
     // return a+b  this shows error so try below
-    if (typeof a === 'number' && typeof b === 'number') {
+    if (typeof a === "number" && typeof b === "number") {
         return a + b;
     }
     else {
@@ -62,13 +67,48 @@ var dataLt = "abc"; // but here you can only apply two types of values (string/n
 //  if many numbers use literals
 // https://www.youtube.com/watch?v=hgtYclE95sc&t=1s  - check vid 14
 function combineMe(a, b, type) {
-    // return a+b  this shows error so try below
-    if (type === 'as-number') {
-        return (+a) + (+b);
+    if (type === "as-number") {
+        return +a + +b;
     }
     else {
         return a.toString() + b.toString();
     }
 }
-console.log(combineMe(10, 20, "as-number"));
-console.log(combineMe("Floy", "Manuel", "as-string"));
+var a = 20;
+var b = "hello";
+var c = undefined;
+var d = true;
+// works.......!!!!
+// ******************************************************************
+// Function Type
+// we use function type to get correct return type-value
+function add() {
+    return "yo";
+} // string type function  - hover over "function"
+function add2() {
+    return 203;
+} // numeric type function
+// How to define...??
+function add3() {
+    return 50;
+    // return "hello" // if you pass string it shows error
+}
+function add4(a, b) {
+    return a + b;
+    //   return a+b+"yo bro" //error
+}
+// the add4 function only defines a numeric type return nothing else
+console.log(add4(25, 50));
+// lets try something new
+var ab = 20;
+var ac = "hello";
+function add5() {
+    // return ac; //error cz numeric type function
+    return ab;
+}
+// ******************************************************************
+// Void 
+// when a function is returning nothing it is void type function 
+function printData() {
+    console.log("hello brother");
+} // this functions returns nothing so this is void type function
--- Vid-9/app.ts
+++ Vid-9/app.ts
@@ -1,35 +1,42 @@
 // Array type in TypeScript
 
-let arr:string[] = ["php", "js", "ts", "node"]; //string array - :string[]
+let arr: string[] = ["php", "js", "ts", "node"]; //string array - :string[]
 // let arr2:string[] | number[] = ["php", "js", "ts", "node"]; //string/number array - :string[] | number []
 
 // let randomNum = ["abc", 123, false] //all mix so
-let random:[string, number, boolean] = ["abc", 123, false];
+let random: [string, number, boolean] = ["abc", 123, false];
 // what if you have many
-// so 
-let random2:any[] = ["abc", 123, false];
+// so
+let random2: any[] = ["abc", 123, false];
 
 // **********************************************************
 
 // Tuple - vid 10
-// Tuple means - you specify all 
-let role:[string, string, number, number] = ['admin', 'manager', 2 , 5];
+// Tuple means - you specify all
+let role: [string, string, number, number] = ["admin", "manager", 2, 5];
 // but if you wanna edit it
 // role[0] = 123; //error cz only string  can pass at position[0]
 role[1] = "hello"; // it can pass
 
 // to push
-let role2:[string, string, number, number, boolean?] = ['admin', 'manager', 2 , 5];
+let role2: [string, string, number, number, boolean?] = [
+  "admin",
+  "manager",
+  2,
+  5,
+];
 role2.push(true);
 // console.log(role2)
 
 // **********************************************************
 
 // Enum - vid-11
-// enum - gives number to every string/value 
+// enum - gives number to every string/value
 
-enum Role{
-    admin, manager, read_only_user
+enum Role {
+  admin,
+  manager,
+  read_only_user,
 }
 
 // this is used cz when you jus type Role for further reference it automatically pops up - Role.
@@ -41,25 +48,25 @@ enum Role{
 
 // ANY type in TypeScript
 // any means any kind of data type can come...
-let data:any = 10;
+let data: any = 10;
 data = "hello";
 // you can also define any as array....   but in js has more chances of getting error
-let data2:any[] = [20, "yo yo"];
+let data2: any[] = [20, "yo yo"];
 
 // ******************************************************************
 
 // union Type in Typescript
 // union type means assigning two or more types
-let data10 : number | string = 10; //example
+let data10: number | string = 10; //example
 
-function combine(a:number | string, b:number |string){
-    // return a+b  this shows error so try below
+function combine(a: number | string, b: number | string) {
+  // return a+b  this shows error so try below
 
-    if(typeof a === 'number' && typeof b === 'number'){
-        return a+b;
-    }else {
-        return a.toString() + b.toString();
-    }
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else {
+    return a.toString() + b.toString();
+  }
 }
 // console.log(combine(10,20))
 // console.log(combine("Floy","Manuel"))
@@ -70,24 +77,92 @@ function combine(a:number | string, b:number |string){
 
 //  better use union  than any cz some benefits you won't receive in any.
 
-
 // ******************************************************************
 
 // Literal Type in TS
 // Actual type is written instead of union...  like
- let dataLt:"abc" | 123 = "abc"// but here you can only apply two types of values (string/number)
+let dataLt: "abc" | 123 = "abc"; // but here you can only apply two types of values (string/number)
 
 //  if many numbers use literals
 // https://www.youtube.com/watch?v=hgtYclE95sc&t=1s  - check vid 14
 
-function combineMe(a:number | string, b:number |string, type: "as-number" | "as-string"){
-    // return a+b  this shows error so try below
-
-    if(type ===  'as-number'){
-        return (+a)+(+b);
-    }else {
-        return a.toString() + b.toString();
-    }
+function combineMe(
+  a: number | string,
+  b: number | string,
+  type: "as-number" | "as-string"
+) {
+  if (type === "as-number") {
+    return +a + +b;
+  } else {
+    return a.toString() + b.toString();
+  }
 }
-console.log(combineMe(10, 20, "as-number"))
-console.log(combineMe("Floy", "Manuel", "as-string"))
+// console.log(combineMe(10, 20, "as-number"));
+// console.log(combineMe("Floy", "Manuel", "as-string"));
+
+// ******************************************************************
+
+// Type Alias in TS
+// writing common type for below
+
+// let a : string|number|undefined|boolean = 20;
+// let b : string|number|undefined|boolean = "hello";
+// let c : string|number|undefined|boolean = undefined;
+// let d : string|number|undefined|boolean = true;
+
+// instead of writng similar code for all do alias
+
+type anyNameType = string | number | undefined | boolean;
+let a: anyNameType = 20;
+let b: anyNameType = "hello";
+let c: anyNameType = undefined;
+let d: anyNameType = true;
+// works.......!!!!
+
+// ******************************************************************
+
+// Function Type
+// we use function type to get correct return type-value
+
+function add() {
+  return "yo";
+} // string type function  - hover over "function"
+function add2() {
+  return 203;
+} // numeric type function
+// How to define...??
+
+function add3(): number {
+  return 50;
+  // return "hello" // if you pass string it shows error
+}
+
+
+function add4(a: number, b: number): number {
+  return a + b;
+//   return a+b+"yo bro" //error
+}
+// the add4 function only defines a numeric type return nothing else
+console.log(add4(25, 50));
+
+// lets try something new
+const ab = 20;
+const ac = "hello";
+function add5():number{
+    // return ac; //error cz numeric type function
+    return ab;
+}
+
+// ******************************************************************
+
+
+// Void - function
+// when a function is returning nothing it is void type function 
+let today;
+function printData(){
+    console.log("hello brother")
+    today = new Date;
+} // this functions returns nothing so this is void type function
+
+
+// ******************************************************************

c5a939068681192b007c203e0810b82f58289a58
Floyd Manuel <floydmanuel011@gmail.com>
2021-10-06 10:07:03 +0530

    include and exclude files for compiling

--- Vid-20/work.js
+++ Vid-20/work.js
@@ -4,3 +4,11 @@ console.log("hello");
 // How you compile both Work.ts and dummy.ts ??? 
 // 1. try individual //but it will be lengthy so try next
 // 2.   @terminal.... tsc -init
+// *************************************************************
+// compiling few out of many.... vid 21
+// write the below code in tsconfig.json at below
+// "exclude": [
+//     "--filename--.ts"
+// ]
+// In the file i excluded dum1.ts file so it didn't get compiled
+// *************************************************************
--- Vid-20/work.ts
+++ Vid-20/work.ts
@@ -5,3 +5,16 @@ console.log("hello");
 // 1. try individual //but it will be lengthy so try next
 // 2.   @terminal.... tsc -init
  
+// *************************************************************
+
+// compiling few out of many.... vid 21
+// write the below code in tsconfig.json at below
+
+// "exclude": [
+//     "--filename--.ts"
+// ]
+
+// In the file i excluded dum1.ts file so it didn't get compiled
+
+
+// *************************************************************
